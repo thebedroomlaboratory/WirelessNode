cd054148648d2396709255fc5ecadbe056539d50
U-disney-2\disney <disney@disney-2.(none)>
2014-07-19 16:09:48 +0100

    Very rough first commit for arduino wireless sensors. The structure of the sketch is that there should be a profile for each type of sensor and each node is setup for wireless connectivity too, on the nrf24l01 mesh.
    Code needs a good bit of tidying and fixing but thought it might be good to commit before it gets deleted accidentally or something.

---
+++ nrf24WirelessNode/nodeconfig.cpp
@@ -0,0 +1,127 @@
+/*
+ Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#include "RF24Network_config.h"
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+#include "eeprom_update_block.h"
+#include "nodeconfig.h"
+
+// Where in EEPROM is the address stored?
+uint8_t* address_at_eeprom_location = (uint8_t*)10;
+
+eeprom_info_t eeprom_info;
+
+const eeprom_info_t& nodeconfig_read(void)
+{
+  eeprom_read_block(&eeprom_info,address_at_eeprom_location,sizeof(eeprom_info));
+
+  // Look for the token in EEPROM to indicate the following value is
+  // a validly set node address 
+  if ( eeprom_info.isValid() ) 
+  {
+    printf_P(PSTR("ADDRESS: %o\n\r"),eeprom_info.address);
+    printf_P(PSTR("ROLE: %S\n\r"),eeprom_info.relay ? PSTR("Relay") : PSTR("Leaf") );
+    printf_P(PSTR("PROFILE: %04x\n\r"),eeprom_info.profile); 
+  }
+  else
+  {
+    eeprom_info.clear();
+
+    printf_P(PSTR("*** No valid address found.  Send node address via serial of the form 011N\n\r"));
+    while(1)
+    {
+      nodeconfig_listen();
+    }
+  }
+  
+  return eeprom_info;
+}
+
+char serialdata[10];
+char* nextserialat = serialdata;
+const char* maxserial = serialdata + sizeof(serialdata) - 1;
+
+void nodeconfig_listen(void)
+{
+  //
+  // Listen for serial input, which is how we set the address
+  //
+  if (Serial.available())
+  {
+    // If the character on serial input is in a valid range...
+    char c = tolower(Serial.read());
+    if ( c >= '0' && c <= '9' ) 
+    {
+      *nextserialat++ = c;
+      if ( nextserialat == maxserial )
+      {
+	*nextserialat = 0;
+	printf_P(PSTR("\r\n*** Unknown serial command: %s\r\n"),serialdata);
+	nextserialat = serialdata;
+      }
+    }
+    else if ( tolower(c) == 'a' )
+    {
+      // Profile for Network Gateway on Galileo
+      set_profile(0);
+      set_address();
+    }
+    else if ( tolower(c) == 'b' )
+    {
+      set_profile(1);
+      set_address();
+    }
+    else if ( tolower(c) == 'c' )
+    {
+      // Profile for Power Strip
+      *nextserialat = 0;
+      int8_t val = strtol(serialdata,NULL,16);
+      nextserialat = serialdata;
+      
+//      set_temp_calibration( val * 0x10 );
+      
+      printf_P(PSTR("RESET NODE before changes take effect\r\n"));
+      if ( ! eeprom_info.isValid() )
+	printf_P(PSTR("Please assign an address\r\n"));
+    }
+    else if ( c == 'd' )
+    {
+      // Profile for Door Contact 
+      set_address();
+    }
+  }
+}
+
+void set_address()
+{
+  char *pc = serialdata;
+  uint16_t address = 0;
+  while ( pc < nextserialat )
+  {
+    address <<= 3;
+    address |= (*pc++ - '0');
+  }
+
+  // It is our address
+  eeprom_info.address = address;
+  eeprom_update_block(&eeprom_info,address_at_eeprom_location,sizeof(eeprom_info));
+
+  // And we are done right now (no easy way to soft reset)
+  printf_P(PSTR("\n\rManually set to address 0%o\n\rPress RESET to continue!"),address);
+  while(1);
+}
+
+void set_profile(int16_t val)
+{
+  eeprom_info.profile = val;
+  printf_P(PSTR("TEMP: %02x\n\r"),eeprom_info.profile);
+  eeprom_update_block(&eeprom_info,address_at_eeprom_location,sizeof(eeprom_info));
+}
+
+// vim:ai:cin:sts=2 sw=2 ft=cpp
---
+++ nrf24WirelessNode/nrf24WirelessNode.ino
@@ -0,0 +1,128 @@
+/*
+    Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 2 as published by the Free Software Foundation.
+    */
+
+/**
+    * Simplest possible example of using RF24Network
+    *
+    * TRANSMITTER NODE
+    * Every 2 seconds, send a payload to the receiver node.
+    */
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include "nodeconfig.h"
+#include "printf.h"
+
+// nRF24L01(+) radio attached using Getting Started board
+RF24 radio(9, 10);
+
+// Network uses that radio
+RF24Network network(radio);
+
+// Our node configuration 
+eeprom_info_t this_node;
+uint8_t has_recent_system_config = false;
+
+// Address of the other node
+const uint16_t other_node = 00;
+
+// How often to send 'hello world to the other unit
+const unsigned long interval = 2000; //ms
+
+// When did we last send?
+unsigned long last_sent;
+
+// How many have we sent already
+unsigned long packets_sent;
+
+// Structure of our payload
+struct payload_t
+{
+    unsigned long ms;
+    unsigned long counter;
+};
+
+//TMP36 Pin Variables
+int sensorPin = 1; //the analog pin the TMP36's Vout (sense) pin is connected to
+//the resolution is 10 mV / degree centigrade with a
+//500 mV offset to allow for negative temperatures
+int sensorPin1 = 0;
+int temperatureCOUT = 0;
+
+void setup(void)
+{
+    Serial.begin(57600);
+    printf_begin();
+    printf_P(PSTR("\n\rRF24Network/examples/helloworld_tx/\n\r"));
+    // Which node are we?
+    this_node = nodeconfig_read();
+    SPI.begin();
+    radio.begin();
+    network.begin(/*channel*/ 90, /*node address*/ this_node.address);
+}
+
+void loop(void)
+{
+    // Pump the network regularly
+    network.update();
+    switch (this_node.profile)
+    {
+    case 0:
+      if( network.available() )
+      {
+        // If so, grab it and print it out
+        RF24NetworkHeader header;
+        payload_t payload;
+        network.read(header,&payload,sizeof(payload));
+        printf_P(PSTR("From 0%o Received packet #%d at %d"),header.from_node,payload.counter,payload.ms);
+      }
+      break;
+    case 1:
+      sendReading();
+      break;
+    default: 
+      // if nothing else matches, do the default
+      // default is optional
+      break;
+    }    
+    nodeconfig_listen();
+}
+
+void sendReading()
+{
+    // If it's time to send a message, send it!
+  unsigned long now = millis();
+  if ( now - last_sent >= interval  )
+  {
+      last_sent = now;       
+      //getting the voltage reading from the temperature sensor
+      int reading = analogRead(sensorPin);
+      // converting that reading to voltage, for 3.3v arduino use 3.3
+      float voltage = reading * 3.3;
+      voltage /= 1024.0;
+
+      int reading2 = analogRead(sensorPin1);  
+      // now print out the temperature
+      float temperatureC = ((voltage - 0.5) * 100) + 24 ; //converting from 10 mv per degree wit 500 mV offset
+      //to degrees ((volatge - 500mV) times 100)
+      temperatureCOUT = (temperatureC * 100);
+      // now convert to Fahrenheight
+      float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+
+      printf_P(PSTR("Sending...\n\r"));
+      payload_t payload = { reading2, temperatureCOUT };
+      RF24NetworkHeader header(/*to node*/ other_node);
+      bool ok = network.write(header, &payload, sizeof(payload));
+      if (ok)
+          printf_P(PSTR("ok.\n\r"));
+      else
+          printf_P(PSTR("failed.\n\r"));
+  }
+}
+
